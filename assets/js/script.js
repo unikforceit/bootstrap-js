c2053e7a3103ee02acf8cbe89bc53295052669b4
Md Rakibul Islam <freelancerakibul@gmail.com>
2022-07-05 14:21:30 +0600

    tabs done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,28 @@
 // $('.btn-primary')
-function myfunction(){
-    alert('Hello');
-}
+// function myfunction(){
+//     alert('Hello');
+// }
 
-$(document).ready(function(){
-// myfunction();
-    var card = '.card';
-    var speed = 1000;
-    $('.btn-primary').click(function(){
-        console.log('clicked in console');
-        // alert('CLicked');
-        // $('.card').hide();
-        // $('.card').show();
-        // $('.card').toggle();
-         $(card).slideToggle(speed);
-    });
+// $(document).ready(function(){
+// // myfunction();
+//     var card = '.card';
+//     var speed = 1000;
+//     $('.btn-primary').click(function(){
+//         console.log('clicked in console');
+//         // alert('CLicked');
+//         // $('.card').hide();
+//         // $('.card').show();
+//         // $('.card').toggle();
+//          $(card).slideToggle(speed);
+//     });
 
-    $('.btn-primary').mouseover(function(){
-        console.log('clicked in console');
-        // alert('CLicked');
-        // $('.card').hide();
-        // $('.card').show();
-        // $('.card').toggle();
-    });
+//     $('.btn-primary').mouseover(function(){
+//         console.log('clicked in console');
+//         // alert('CLicked');
+//         // $('.card').hide();
+//         // $('.card').show();
+//         // $('.card').toggle();
+//     });
 
-});
+// });
 
